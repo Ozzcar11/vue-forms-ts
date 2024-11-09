cbeeb7e52856a649e4902a744683ddea60eaab26
Atav <atav.daniyalov01@gmail.com>
2024-11-09 22:48:41 +0300

    feat: добавил компонент header

---
+++ src/components/app/app-header.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__logo">
+        <img src="/icons/logo.svg" alt="Alef Logo" />
+      </div>
+      <div class="header__menu">
+        <router-link to="/" class="header__link">Форма</router-link>
+        <router-link to="/preview" class="header__link">Превью</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: relative;
+  border-bottom: 1px solid $colors-gray-xs;
+
+  &__container {
+    display: flex;
+    align-items: center;
+    padding: 7px 92px;
+  }
+
+  &__logo {
+    position: absolute;
+  }
+
+  &__link {
+    @include p2-regular;
+
+    color: $colors-gray;
+    position: relative;
+    display: flex;
+    align-items: center;
+    height: 70px;
+  }
+
+  &__menu {
+    display: flex;
+    justify-content: center;
+    gap: 24px;
+    width: 100%;
+  }
+}
+</style>
