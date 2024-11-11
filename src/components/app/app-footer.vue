d1328183ad5cb3677cc73c63f35eae846a20f970
Atav <atav.daniyalov01@gmail.com>
2024-11-11 16:45:45 +0300

    feat: Добавил компонент footer

---
+++ src/components/app/app-footer.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <footer class="footer">all rights reserved</footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  @include p3-regular;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 0;
+  background-color: $colors-gray-xs;
+}
+</style>
